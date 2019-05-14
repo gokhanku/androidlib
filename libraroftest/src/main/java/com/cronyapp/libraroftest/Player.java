853f4502512bdea205ef4dff7ae3d2f693005462
gokhanku <gokhan.kurt@outlook.com>
2019-05-14 05:07:42 +0300

    Activate Library test 2

--- libraroftest/src/main/java/com/cronyapp/libraroftest/Player.java
+++ libraroftest/src/main/java/com/cronyapp/libraroftest/Player.java
@@ -4,6 +4,31 @@ public class Player {
 
     String name;
     String surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
     int playerId;
 
 }
